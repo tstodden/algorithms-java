4877ce81645fc9a70e747ef03dcf2f28d6e0a68d
Tyler Stodden <tylersstodden@gmail.com>
2022-10-02 18:03:57 -0400

    Add collinear tests data and client to BruteCollinearPoints

--- src/main/io/tstodden/algorithms/collinear/BruteCollinearPoints.java
+++ src/main/io/tstodden/algorithms/collinear/BruteCollinearPoints.java
@@ -1,5 +1,8 @@
 package io.tstodden.algorithms.collinear;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -80,5 +83,35 @@ public class BruteCollinearPoints {
       return false;
     }
     return Math.abs(x - y) < ACCEPTABLE_DELTA;
+  }
+
+  public static void main(String[] args) {
+
+    // read the n points from a file
+    In in = new In(args[0]);
+    int n = in.readInt();
+    Point[] points = new Point[n];
+    for (int i = 0; i < n; i++) {
+      int x = in.readInt();
+      int y = in.readInt();
+      points[i] = new Point(x, y);
+    }
+
+    // draw the points
+    StdDraw.enableDoubleBuffering();
+    StdDraw.setXscale(0, 32768);
+    StdDraw.setYscale(0, 32768);
+    for (Point p : points) {
+      p.draw();
+    }
+    StdDraw.show();
+
+    // print and draw the line segments
+    BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+    for (LineSegment segment : collinear.segments()) {
+      StdOut.println(segment);
+      segment.draw();
+    }
+    StdDraw.show();
   }
 }
